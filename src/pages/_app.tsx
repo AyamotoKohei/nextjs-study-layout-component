a2ca42245778d157af9866fbfc610bc5124a6761
AyamotoKohei <koh14taka2@gmail.com>
2022-01-01 23:38:17 +0900

    add: グローバルレイアウトの実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  const getLayout = Component.getLayout || ((page) => page);
 
-export default MyApp
+  return getLayout(<Component {...pageProps} />);
+};
+
+export default MyApp;
